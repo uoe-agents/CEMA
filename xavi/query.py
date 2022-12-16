d2528bd510b06c39c93554f6dc2334c839e4dd20
ChengWang <18771014894@163.com>
2022-12-16 00:47:33 +0000

    remove common action for whynot

--- xavi/query.py
+++ xavi/query.py
@@ -177,6 +177,17 @@ class Query:
                         else:
                             action_statistic[key] += 1
                 segmentations.append(action_segmentations)
+                # substract the action if repeated to remove common action, code need optimized
+                for action_seg in action_segmentations:
+                    for seg in observation_segmentations:
+                        for time in seg.times:
+                            if time > action_seg.times[-1]:
+                                break
+                            elif time < action_seg.times[0]:
+                                continue
+                            key = tuple(seg.actions)
+                            if key == tuple(action_seg.actions):
+                                action_statistic[key] -= 1
             self.__longest_action = max(action_statistic, key=action_statistic.get)
 
             longest_action_exist = False
