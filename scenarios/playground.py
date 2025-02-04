774d73e0492c1442642d909e8284820bb631a447
Balint Gyevnar <gyevnarb@gmail.com>
2025-02-04 14:42:58 +0000

    Added pyproject.toml

--- scenarios/playground.py
+++ scenarios/playground.py
@@ -1,62 +1,19 @@
 import logging
 import sys
 import argparse
-import tqdm
-import pandas as pd
-from evaluation import load_scenario
-from plotting import plot_sampling_results, plot_distribution_results, plot_explanation
+
+
+from matplotlib import pyplot as plt
+import igp2 as ip
 
 logger = logging.Logger(__name__)
 
 
 
 def main(args):
-    agent, query = load_scenario(args.sid, args.qid)
-    query_str = f"n30_t{query.t_query}_m{query.type}"
-
-    # sampling_size_results = {}
-    # for sample_size in (pbar := tqdm.tqdm([100, 250, 500, 750, 1000])):
-    #     results = []
-    #     logger.info("Generating explanations for sample size %d", sample_size)
-    #     agent._cf_n_samples = sample_size
-    #     causes = agent.explain_actions(query)
-    #     results.append(causes)
-    #     sampling_size_results[sample_size] = results
-    # plot_sampling_results(sampling_size_results, "output/test", query_str)
-
-    causes = agent.explain_actions(query)
-    dataset_backup = agent.cf_datasets.copy()
-    agent.cf_datasets["tau"] = {k: v for k, v in dataset_backup["tau"].items() if v.occluded_factor.no_occlusions}
-    agent.cf_datasets["t_action"] = {k: v for k, v in dataset_backup["t_action"].items() if v.occluded_factor.no_occlusions}
-    causes_no_occlusions = agent.explain_actions(query)
-    agent.cf_datasets["tau"] = {k: v for k, v in dataset_backup["tau"].items() if not v.occluded_factor.no_occlusions}
-    agent.cf_datasets["t_action"] = {k: v for k, v in dataset_backup["t_action"].items() if not v.occluded_factor.no_occlusions}
-    causes_occlusions = agent.explain_actions(query)
-    plot_explanation(causes_no_occlusions[0], causes_no_occlusions[1][0:2], "noc_" + query_str, "output/test")
-    plot_explanation(causes_occlusions[0], causes_occlusions[1][0:2], "oc_" + query_str, "output/test")
-
-    # teleological_causes = causes[0]
-    # occluded_past_present = []
-    # for item in agent.cf_datasets["tau"].values():
-    #     for reward in item.rewards:
-    #         occluded_past_present.append(not item.occluded_factor.no_occlusions)
-    # occluded_past_present = pd.Series(occluded_past_present)
-    # teleological_past_occluded = teleological_causes[0][1][occluded_past_present]
-    # teleological_past_notoccluded = teleological_causes[0][1][~occluded_past_present]
-    # mechanistic_causes = causes[1]
-    # occluded_past_present = mechanistic_causes[2][0]["2_occluded"].astype(bool)
-
-    # distribution_results = {}
-    # agent._cf_n_samples = 100
-    # for alpha in (pbar := tqdm.tqdm([0.01, 0.1, 0.5, 1.0])):
-    #     results = []
-    #     pbar.set_description(f"Generating explanations for distribution smoothing alpha {alpha:.3f}")
-    #     agent._alpha = alpha
-    #     for i in range(10):
-    #         causes = agent.explain_actions(query)
-    #         results.append(causes)
-    #     distribution_results[alpha] = results
-    # plot_distribution_results(distribution_results, "output/test", query_str)
+    scenario_map = ip.Map.parse_from_opendrive("scenarios/maps/scenario1.xodr")
+    ip.plot_map(scenario_map, midline=True)
+    plt.show()
 
 
 if __name__ == '__main__':
--- setup.py
+++ setup.py
@@ -6,15 +6,15 @@ with open('requirements.txt') as f:
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
-setuptools.setup(name='xavi',
-                 version='1.0',
+setuptools.setup(name='cema',
+                 version='0.1.0',
                  description='Supporting code to the paper "Causal Social Explanations for '
                              'Stochastic Sequential Multi-Agent Decision-Making"',
                  long_description=long_description,
                  long_description_content_type="text/markdown",
                  author='Balint Gyevnar, Cheng Wang',
                  author_email='balint.gyevnar@ed.ac.uk',
-                 url='https://github.com/uoe-agents/xavi',
+                 url='https://github.com/uoe-agents/cema',
                  packages=setuptools.find_packages(exclude=["scenarios"]),
                  install_requires=requirements
                  )
